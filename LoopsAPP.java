446cf3f022fb31aa466783db49e4f5a700ab50e7
Arianna0209 <agendres@students.unwsp.edu>
2026-02-20 09:29:19 -0500

    Add files via upload

---
+++ LoopsAPP.java
@@ -0,0 +1,12 @@
+package week5;
+
+public class LoopsAPP {
+
+	public static void main(String[] args) {
+		int i;
+		for (i = 1; i < 5000; i = 2 * i + 1) {
+			System.out.println("Count is: " + i);
+		}
+	}
+
+}
